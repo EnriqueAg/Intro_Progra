d974c3f9f127f2ee813d736a04fc2d269e359e8e
EnriqueAg <32582615+EnriqueAg@users.noreply.github.com>
2017-11-25 16:54:01 -0600

    Add files via upload

--- Práctica_4/Ejercicio_5/main.c
+++ Práctica_4/Ejercicio_5/main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
-  
-  system("PAUSE");	
+  double l[2];
+  int x;
+  int y=0;
+  double h;
+  for(x=1;x<=3;x++){
+  printf("\n\tTriangulo %d\n");
+  printf("Ingrese lado 1: ");
+  scanf("%lf",&l[y]);
+  printf("Ingrese lado 2: ");
+  scanf("%lf",&l[y+1]);
+  h=hipotenusa(l[y],l[y+1]);
+  printf("Hipotenusa: %f",h);
+
+  }
+  system("PAUSE");
   return 0;
+}
+
+int hipotenusa(double x,double y){
+double h;
+x=x*x;
+y=y*y;
+h=sqrt(x+y);
+return h;
 }
